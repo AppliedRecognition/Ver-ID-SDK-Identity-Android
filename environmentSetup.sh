6e1aebf0421fb927e65d979aacbfd9e602631eb7
Jakub Dolejs <jakubdolejs@gmail.com>
2020-03-05 18:00:04 +0000

    Added missing environment variables in script

--- environmentSetup.sh
+++ environmentSetup.sh
@@ -15,3 +15,11 @@ echo "Writing githubUsername to gradle.properties..."
 echo "githubUsername=${1}" >> $GRADLE_PROPERTIES
 echo "Writing githubAccessToken to gradle.properties..."
 echo "githubAccessToken=${2}" >> $GRADLE_PROPERTIES
+echo "Writing verid_artifactory_url to gradle.properties..."
+echo "verid_artifactory_url=${3}" >> $GRADLE_PROPERTIES
+echo "Writing verid_artifactory_repo to gradle.properties..."
+echo "verid_artifactory_repo=${4}" >> $GRADLE_PROPERTIES
+echo "Writing verid_artifactory_username to gradle.properties..."
+echo "verid_artifactory_username=${5}" >> $GRADLE_PROPERTIES
+echo "Writing verid_artifactory_password to gradle.properties..."
+echo "verid_artifactory_password=${6}" >> $GRADLE_PROPERTIES
